6b48256a025346e16dc6332fd019a5a141ab9e92
Sebson <sirmcmuffin95@gmail.com>
2017-09-21 14:17:51 +0200

    Added trace function to fetch bandwidth from log file

--- main/src/main.java
+++ main/src/main.java
@@ -1,5 +1,26 @@
-/**
- * Created by David on 2017-09-21.
- */
+import java.util.*;
+import java.io.*;
+import java.net.URL;
+
 public class main {
+
+    public static void main(String[] args) throws IOException {
+    }
+
+    private static Vector trace() throws IOException {
+        URL traceUrl = new URL("http://www.ida.liu.se/~TDDD66/labs/2017/report.2011-01-30_1323CET.log");
+        Scanner s = new Scanner(traceUrl.openStream());
+        Vector bandwidth = new Vector();
+        String row;
+        int traceWidth;
+
+        while(s.hasNextLine()){
+            row = s.nextLine();
+            String[] parts = row.split(" ");
+            traceWidth = Integer.parseInt(parts[4]);
+            bandwidth.add(traceWidth);
+        }
+
+        return bandwidth;
+    }
 }
