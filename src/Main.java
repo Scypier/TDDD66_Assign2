4e074ed55502c352a6c196e1db69a323564e4188
Sebson <sirmcmuffin95@gmail.com>
2017-10-03 13:48:44 +0200

    DONE

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,31 @@ import java.net.URL;
 
 public class Main {
 
+    /**
+     * Creates and runs a player
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         Player player = new Player(4, 6, 2);
         player.run(trace());
     }
 
+    /**
+     * Retrieves the log file and extracts the bandwidth column
+     * @return Vector of bandwidth in bps. Each element represents a new second.
+     * @throws IOException
+     */
+
     private static Vector<Integer> trace() throws IOException {
         Scanner s = new Scanner(new URL("http://www.ida.liu.se/~TDDD66/labs/2017/report.2011-01-30_1323CET.log").openStream());
         Vector<Integer> bandwidth = new Vector();
 
-        while(s.hasNextLine()){
+        while(s.hasNextLine()){ // Extracts the correct column and converts the value to bps
             String[] parts = s.nextLine().split(" ");
             bandwidth.add(Integer.parseInt(parts[4])*8);
         }
 
         return bandwidth; // Expressed in bits per second
-    }
+
 }
